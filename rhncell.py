cc8fe58df4c6d3229c174f1cda585ab8d55734d2
Dendi Suhubdy <suhubdyd@iro.umontreal.ca>
2017-08-30 12:28:09 -0400

    adding research ideas: modifying tanh on the non-input gates

--- rhncell.py
+++ rhncell.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function
 import tensorflow as tf
-import numpy as np
 
 from tensorflow.python.ops import variable_scope as vs
 from tensorflow.python.ops import math_ops, array_ops
@@ -55,7 +54,7 @@ class RHNCell(RNNCell):
         else:
         # the if else is the indicator function
         # basically this is where h is computed based on the current state
-          h = tf.tanh(linear([current_state * noise_h], self._num_units, True))
+          h = tf.tanh(linear([current_state * noise_h], self._num_units, False))
       with tf.variable_scope('t_'+str(i)):
         if i == 0:
           t = tf.sigmoid(linear([inputs * noise_i, current_state * noise_h], self._num_units, True, self.forget_bias))
@@ -64,9 +63,9 @@ class RHNCell(RNNCell):
       # we start by modifying this part
       # since we want to return the current state that would be 
       if i == 0:
-        current_state = (h + current_state)
-      else:
         current_state = (h - current_state)* t + current_state
+      else:
+        current_state = (h + current_state)
 
     return current_state, [current_state, noise_i, noise_h]
 
@@ -90,6 +89,8 @@ def linear(args, output_size, bias, bias_start=None, scope=None):
 
   Raises:
     ValueError: if some of the arguments has unspecified or wrong shape.
+  
+  This is still needed for c, but not for h in i>0
   """
   if args is None or (nest.is_sequence(args) and not args):
     raise ValueError("`args` must be specified")
@@ -125,3 +126,74 @@ def linear(args, output_size, bias, bias_start=None, scope=None):
       bias_term = vs.get_variable("Bias", [output_size], dtype=dtype,
                                   initializer=tf.constant_initializer(bias_start, dtype=dtype))
   return res + bias_term
+
+
+def linear_non_inputcell(args, output_size, bias, bias_start=None, scope=None):
+  """
+  Used when i > 0
+
+  This is a slightly modified version of _linear used by Tensorflow rnn.
+  The only change is that we have allowed bias_start=None.
+
+  Linear map: sum_i(args[i] * W[i]), where W[i] is a variable.
+
+  Args:
+    args: a 2D Tensor or a list of 2D, batch x n, Tensors.
+    output_size: int, second dimension of W[i].
+    bias: boolean, whether to add a bias term or not.
+    bias_start: starting value to initialize the bias; 0 by default.
+    scope: VariableScope for the created subgraph; defaults to "Linear".
+
+  Returns:
+    A 2D Tensor with shape [batch x output_size] equal to
+    sum_i(args[i] * W[i]), where W[i]s are newly created matrices.
+
+  Raises:
+    ValueError: if some of the arguments has unspecified or wrong shape.
+  
+  """
+  
+  if args is None or (nest.is_sequence(args) and not args):
+    raise ValueError("`args` must be specified")
+  if not nest.is_sequence(args):
+    args = [args]
+
+  # Calculate the total size of arguments on dimension 1.
+  total_arg_size = 0
+  
+  shapes = [a.get_shape().as_list() for a in args]
+  
+  for shape in shapes:
+    if len(shape) != 2:
+      raise ValueError("Linear is expecting 2D arguments: %s" % str(shapes))
+    if not shape[1]:
+      raise ValueError("Linear expects shape[1] of arguments: %s" % str(shapes))
+    else:
+      total_arg_size += shape[1]
+
+  dtype = [a.dtype for a in args][0]
+
+  # Now the computation.
+  with vs.variable_scope(scope or "Linear"):
+    weights = vs.get_variable(
+                                "Matrix", 
+                                [total_arg_size, output_size], 
+                                dtype=dtype)
+    
+    # this is a dot product operation
+    #  args[0] is the input vector
+    
+    res_h = math_ops.matmul(args[0], weights)
+    
+    if not bias:
+      return res_h
+    elif bias_start is None:
+      bias_term_r = vs.get_variable("Bias", 
+                                [output_size], 
+                                dtype=dtype)
+    else:
+      bias_term_r = vs.get_variable("Bias", 
+                                [output_size], 
+                                dtype=dtype,
+                                initializer=tf.constant_initializer(bias_start, dtype=dtype))
+  return res_h + bias_term_r
